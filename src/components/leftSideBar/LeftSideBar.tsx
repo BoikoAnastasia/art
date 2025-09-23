a030fe62063fd47e16b3d5cc84090d428bf26377
Анастасия Бойко <anastasia151000@rambler.ru>
2025-09-23 15:13:20 +0300

    new leftsidebar icons

--- src/components/leftSideBar/LeftSideBar.tsx
+++ src/components/leftSideBar/LeftSideBar.tsx
@@ -2,25 +2,21 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 // icons
 import FormatColorFillIcon from '@mui/icons-material/FormatColorFill';
-import CreateIcon from '@mui/icons-material/Create';
 import BrushIcon from '@mui/icons-material/Brush';
 import ColorizeIcon from '@mui/icons-material/Colorize';
 import HighlightAltIcon from '@mui/icons-material/HighlightAlt';
-import RectangleIcon from '@mui/icons-material/Rectangle';
 import SearchIcon from '@mui/icons-material/Search';
 import CropIcon from '@mui/icons-material/Crop';
-import TitleIcon from '@mui/icons-material/Title';
 import InterestsIcon from '@mui/icons-material/Interests';
 import LoopIcon from '@mui/icons-material/Loop';
-import CircleIcon from '@mui/icons-material/Circle';
 import BackHandIcon from '@mui/icons-material/BackHand';
 import FormatShapesIcon from '@mui/icons-material/FormatShapes';
+
 // images
-import CircleDashed from '../icons/circle-dashed.png';
 import Eraser from '../icons/eraser.png';
 import Lasso from '../icons/lasso.png';
 import Move from '../icons/move.png';
-import Select from '../icons/select.png';
+import Tourch from '../icons/torch.png';
 
 export const LeftSideBar = () => {
   const icons = [
@@ -28,18 +24,14 @@ export const LeftSideBar = () => {
     CropIcon,
     HighlightAltIcon,
     ColorizeIcon,
-    CreateIcon,
     BrushIcon,
     FormatColorFillIcon,
-    TitleIcon,
-    CircleIcon,
-    RectangleIcon,
     InterestsIcon,
     SearchIcon,
     LoopIcon,
     BackHandIcon,
   ];
-  const images = [Move, Lasso, CircleDashed, Select, Eraser];
+  const images = [Move, Lasso, Eraser, Tourch];
 
   return (
     <Box
