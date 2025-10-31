cc07d07af63506eb68b46c3b1a22f16e83ac6944
Анастасия Бойко <anastasia151000@rambler.ru>
2025-10-31 22:47:05 +0300

    Updates

--- src/components/colorPicker/ColorPicker.tsx
+++
@@ -1,7 +0,0 @@
-import { SketchPicker } from 'react-color';
-import { useColor } from '../../contexts/ColorContext';
-
-export const ColorPicker = () => {
-  const { color, setColor } = useColor();
-  return <SketchPicker color={color} onChangeComplete={(c) => setColor(c.hex)} />;
-};
